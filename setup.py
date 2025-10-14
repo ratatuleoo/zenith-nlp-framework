884e2f779ffcfff6c067b3de7e108e1acff217e1
K Satya Sai Nischal <24r11a05c5@gcet.edu.in>
2025-10-14 14:34:49 +0530

    Initial commit of the advanced NLP framework

--- setup.py
+++ setup.py
@@ -1,21 +1,28 @@
 from setuptools import setup, find_packages
-
+    
 with open('requirements.txt') as f:
     requirements = f.read().splitlines()
-
+    
 setup(
     name="zenith-nlp-framework",
-    version="0.1.0",
+    version="1.0.0",
     author="Satya Sai Nischal",
     author_email="coderstale@gmail.com",
-    description="An advanced NLP framework",
+    description="An advanced, from-scratch NLP framework for training and deploying modern transformer models.",
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/cattolatte/zenith-nlp-framework",
     packages=find_packages(where='src'),
     package_dir={"": "src"},
     install_requires=requirements,
     classifiers=[
-        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.9",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
+            "Intended Audience :: Developers",            "Intended Audience :: Science/Research",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
     ],
     python_requires='>=3.9',
 )
+    
+
